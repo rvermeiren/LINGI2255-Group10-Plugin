3250b8bad61366dfc28b50fd58131832eef0808c
Florian Felten <florian.felten@student.uclouvain.be>
2016-10-07 12:33:32 +0200

    built firebreath

---
+++ Browser/WeCitizens/build/projects/WeCitizens/CMakeFiles/WeCitizens.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/WeCitizens.dir/Factory.cpp.o"
+  "CMakeFiles/WeCitizens.dir/WeCitizensAPI.cpp.o"
+  "CMakeFiles/WeCitizens.dir/WeCitizens.cpp.o"
+  "../../bin/WeCitizens/npWeCitizens.pdb"
+  "../../bin/WeCitizens/npWeCitizens.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/WeCitizens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Browser/WeCitizens/build/projects/WeCitizens/PluginAuto/CMakeFiles/WCi_PluginAuto.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/WCi_PluginAuto.dir/PluginInfo.cpp.o"
+  "CMakeFiles/WCi_PluginAuto.dir/precompiled_headers.cpp.o"
+  "CMakeFiles/WCi_PluginAuto.dir/null/NullLogger.cpp.o"
+  "CMakeFiles/WCi_PluginAuto.dir/X11/KeyCodesX11.cpp.o"
+  "CMakeFiles/WCi_PluginAuto.dir/X11/np_x11main.cpp.o"
+  "CMakeFiles/WCi_PluginAuto.dir/X11/PluginWindowX11.cpp.o"
+  "CMakeFiles/WCi_PluginAuto.dir/X11/NpapiPluginX11.cpp.o"
+  "libWCi_PluginAuto.pdb"
+  "libWCi_PluginAuto.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/WCi_PluginAuto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
