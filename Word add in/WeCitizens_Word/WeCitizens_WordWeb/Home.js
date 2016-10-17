76fd2f559109ad4c512da94f3f3f91e4de9fb31e
Jerome Thiry <jr.thiry@gmail.com>
2016-10-17 15:44:52 +0200

    Changed css to look like the website + function is executed every second

--- Word add in/WeCitizens_Word/WeCitizens_WordWeb/Home.js
+++ Word add in/WeCitizens_Word/WeCitizens_WordWeb/Home.js
@@ -12,6 +12,7 @@
     politixxName.push('Zébulon')
     politixxName.push('Anguerrand')
     politixxName.push('téophilémon')
+    politixxName.push('quentin de grolard')
 
 
     // La fonction d'initialisation doit être exécutée chaque fois qu'une nouvelle page est chargée.
@@ -27,7 +28,7 @@
                 $("#template-description").text("Cet exemple affiche le texte sélectionné.");
                 $('#button-text').text("Chercher les politicards");
                 $('#button-desc').text("Afficher le texte sélectionné");
-                
+
                 $('#highlight-button').click(
                     displaySelectedText);
                 return;
@@ -36,14 +37,20 @@
             $("#template-description").text("Cet exemple met en surbrillance les noms contenus dans le texte parmi la liste suivante : Anatole, Zébulon, Anguerrand, téophilémon, Louis.");
             $('#button-text').text("Mettre en surbrillance !");
             $('#button-desc').text("Met en surbrillance les noms de la liste.");
-            
+
             loadSampleData();
 
             // Ajoutez un gestionnaire d'événements Click pour le bouton de mise en surbrillance.
             $('#highlight-button').click(
-                hightlightLongestWord);
+                launchProcess);
         });
     };
+
+    function launchProcess() {
+      hightlightNames();
+      setInterval(hightlightNames, 1000);
+      return ;
+    }
 
     function loadSampleData() {
 
@@ -56,7 +63,7 @@
             // Mettez en file d'attente une commande pour effacer le contenu du corps.
             body.clear();
             // Mettez en file d'attente une commande pour insérer du texte à la fin du corps du document Word.
-            body.insertText("This bLouis a sample text Zébulon Téophilémon inserted in Anatole the anatole document. théophilemnon is not a cool guy lololol",
+            body.insertText("This bLouis guy was writing a sample text but Zébulon and Téophilémon inserted Anatole in anatole.\ntéophilémon is totally not a cool guy. Lol.\nquentin de grolard is a funny guy.",
                             Word.InsertLocation.end);
 
             // Synchronisez l'état du document en exécutant les commandes en file d'attente, puis retournez une promesse pour indiquer l'achèvement de la tâche.
@@ -65,7 +72,7 @@
         .catch(errorHandler);
     }
 
-    function hightlightLongestWord() {
+    function hightlightNames() {
 
         Word.run(function (context) {
 
@@ -76,18 +83,11 @@
             var searchResults = [];
             return context.sync()
             .then(function () {
-                
-                
+
+
                 for (var name in politixxName) {
                     searchResults.push(range.search(politixxName[name], {matchCase:true, matchWholeWord: true }));
                 }
-                //for (var i = 0; i < 26; i++) {
-                //    searchResults.push(range.search('A*', { matchCase: true, matchWholeWord: false, matchWildcards: true }));
-                //}
-
-                /*for (var result in searchResults) {
-                    context.load(result, 'font');
-                }*/
 
                 var arrayLength = searchResults.length;
                 for (var i = 0; i < arrayLength; i++) {
@@ -102,7 +102,7 @@
                 for (var i = 0; i < arrayLength; i++) {
                     var itemLength = searchResults[i].items.length;
                     for (var j = 0; j < itemLength; j++) {
-                           
+
                         searchResults[i].items[j].font.highlightColor = '#FFFF00';
                         searchResults[i].items[j].font.italic = true;
                     }
@@ -111,7 +111,7 @@
             .then(context.sync)
         })
         .catch(errorHandler);
-    } 
+    }
 
 
     function displaySelectedText() {
