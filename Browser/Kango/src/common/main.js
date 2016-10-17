bfb7654f2b0e1830a612245457072c3cbcb3427d
Florian Felten <florian.felten@student.uclouvain.be>
2016-10-17 16:36:48 +0200

    Search method

--- Browser/Kango/src/common/main.js
+++ Browser/Kango/src/common/main.js
@@ -6,16 +6,40 @@
 // ==/UserScript==
 var $ = window.$.noConflict(true); // Required for IE
 
-var title = $(document.createElement('img')).attr({
-    src: 'https://img.pandawhale.com/post-61492-dancing-dickbutt-gif-imgur-tum-pTDg.gif',
-    title: 'Christmas tree'
-}).css({
-    position: 'absolute',
-    top: '10px',
-    left: document.body.clientWidth - 280 + 'px',
-    'z-index': '10000'
-}).appendTo(document.body);
+$(function(){
 
-title.click(function() {
-    alert('BIGGEST DICK IN THE WORLD');
+    // function get_word(text, index) {
+    //     var end = index+1;
+    //     while(text.charAt(end) != ' '){
+    //         end++;
+    //     }
+    //     return text.slice(index, end);
+    // }
+
+    $('p').each(function(index) {
+        var body = $(this).text();
+        var word;
+        var str = "";
+        var reg = /[A-Z]+[a-z]*/gm;
+        while(word = reg.exec(body)){
+            str += word + "\n";
+        }
+        if (str != "")
+            alert(str);
+    });
+
 });
+
+// var title = $(document.createElement('img')).attr({
+//     src: 'https://img.pandawhale.com/post-61492-dancing-dickbutt-gif-imgur-tum-pTDg.gif',
+//     title: 'Christmas tree'
+// }).css({
+//     position: 'absolute',
+//     top: '10px',
+//     left: document.body.clientWidth - 280 + 'px',
+//     'z-index': '10000'
+// }).appendTo(document.body);
+//
+// title.click(function() {
+//     alert('BIGGEST DICK IN THE WORLD');
+// });
