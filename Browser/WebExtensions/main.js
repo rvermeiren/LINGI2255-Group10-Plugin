5db63d87e8badb85e9b022a17be8c33f07704028
Yvhan Smal <yvhan.smal@student.uclouvain.be>
2016-10-26 12:47:45 +0200

    fix

--- Browser/WebExtensions/main.js
+++ Browser/WebExtensions/main.js
@@ -1,14 +1,13 @@
 ﻿// import * as Papa from "node_modules/papaparse/papaparse.min"
 //import * as db_reader from "utils/db_reader";
 
-import * as db_reader from "utils/db_reader";
+//import * as db_reader from "utils/db_reader";
 
 var sarko = "Sarkozy";
 
 $(document).ready(function(){
 
-    var hashmap = db_reader.getHashMap();
-    kango.console.log(hashmap);
+    //var hashmap = db_reader.getHashMap();
 
     //$('head').append("<script type='text/javascript'> $(\".politicianFind\").click(function() {alert(\"WeCitizens\");}); </script>");
 
