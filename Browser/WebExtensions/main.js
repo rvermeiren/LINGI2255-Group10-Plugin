d8a1db72cd1f076fb45784adb161fc13307b2f00
Florian Felten <florian.felten@student.uclouvain.be>
2016-10-26 14:33:56 +0200

    add image link to popup

--- Browser/WebExtensions/main.js
+++ Browser/WebExtensions/main.js
@@ -14,56 +14,110 @@ $(document).ready(function(){
     $('p').each(function(index) {
 
         var body = $(this).html();
-        console.log(body);
+        // console.log(body);
         var word;
         var reg = /[A-Z]+[a-z]*/gm;
         while(word = reg.exec(body)){
 
             if(word == sarko){
-                var image = "<img src=\"https://s15.postimg.org/pei4ci3fv/fdp.png\""
-                + " class=\"politicianFind\" onclick=\"openWeCitizens()\">";
+                var image = "<img alt=\"sarko\" src=\"https://s15.postimg.org/pei4ci3fv/fdp.png\""
+                + " class=\"politicianFind\">";
 
-                $(this).html(body.replace(word, image + " " + word));
+                $(this).html(body.replace(word, word + " " + image));
             }
-            
+
         }
     });
 
     $('li').each(function(index) {
 
         var body = $(this).html();
-        console.log(body);
+        // console.log(body);
         var word;
         var reg = /[A-Z]+[a-z]*/gm;
         while(word = reg.exec(body)){
 
             if(word == sarko){
-                var image = "<img src=\"https://s15.postimg.org/pei4ci3fv/fdp.png\""
-                + " class=\"politicianFind\" onclick=\"alert(word)\">";
+                var image = "<img alt=\"sarko\" src=\"https://s15.postimg.org/pei4ci3fv/fdp.png\""
+                + " class=\"politicianFind\">";
 
-                $(this).html(body.replace(word, image + " " + word));
-            }
-            
-        }
-    });
-
-    $('meta').each(function(index) {
-
-        var body = $(this).html();
-        console.log(body);
-        var word;
-        var reg = /[A-Z]+[a-z]*/gm;
-        while(word = reg.exec(body)){
-
-            if(word == sarko){
-                var image = "<img src=\"https://s15.postimg.org/pei4ci3fv/fdp.png\""
-                + " class=\"politicianFind\" onclick=\"alert(word)\">";
-
-                $(this).html(body.replace(word, image + " " + word));
+                $(this).html(body.replace(word, word + " "+ image));
             }
 
         }
     });
+
+    // $('meta').each(function(index) {
+	//
+    //     var body = $(this).html();
+    //     // console.log(body);
+    //     var word;
+    //     var reg = /[A-Z]+[a-z]*/gm;
+    //     while(word = reg.exec(body)){
+	//
+    //         if(word == sarko){
+    //             var image = "<img alt=\"sarko\" src=\"https://s15.postimg.org/pei4ci3fv/fdp.png\""
+    //             + " class=\"politicianFind\">";
+	//
+    //             $(this).html(body.replace(word, word + " " + image));
+    //         }
+	//
+    //     }
+    // });
+
+
+	function showImage(fullPath){
+		var id = '#dialog';
+
+		//Get the screen height and width
+		var maskHeight = $(window).height();
+		var maskWidth = $(window).width();
+		var fullImagePath = "\"https://s15.postimg.org/pei4ci3fv/fdp.png\"";
+
+
+		//Set heigth and width to mask to fill up the whole screen
+		$('#mask').css({'width':'750px','height':'750px'});
+		$('.image').css({'width':'750px','height':'670px'});
+		$('.image').attr({'src':fullImagePath});
+
+		//transition effect
+		$('#mask').fadeIn(500);
+		$('#mask').fadeTo("slow",0.9);
+
+		//Get the window height and width
+		var winH = $(window).height();
+		var winW = $(window).width();
+
+		//Set the popup window to center
+		$(id).css('top',  '20px');
+		$(id).css('bottom',  '20px');
+		$(id).css('height',  '700px');
+		$(id).css('left', winW/2-$(id).width()/2);
+
+		//transition effect
+		$(id).fadeIn(2000);
+
+		//if close button is clicked
+		$('.window .close').click(function (e) {
+			//Cancel the link behavior
+			e.preventDefault();
+
+			$('#mask').hide();
+			$('.window').hide();
+		});
+
+		//if mask is clicked
+		$('#mask').click(function () {
+			$(this).hide();
+			$('.window').hide();
+		});
+	};
+
+	$(".politicianFind").on("click",function(){
+		console.log($(this).attr("alt"));
+		// window.scrollTo(0,0);
+		showImage($(this).attr("alt"));
+	});
 
 });
 
