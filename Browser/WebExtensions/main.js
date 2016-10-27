ed1d1de1f630bdb1995d16efcf9106307cdc53b2
Jerome Thiry <jr.thiry@gmail.com>
2016-10-27 16:37:12 +0200

    Tried to pass a message... failed.

--- Browser/WebExtensions/main.js
+++ Browser/WebExtensions/main.js
@@ -1,17 +1,11 @@
-﻿// import * as Papa from "node_modules/papaparse/papaparse.min"
-//import * as db_reader from "utils/db_reader";
-
-//import * as db_reader from "utils/db_reader";
-
-var sarko = "Nicolas";
+﻿var sarko = "Nicolas";
 
 $(document).ready(function(){
 	console.log('Document is ready.. scanning for politicians...');
 
-	$('head').prepend('<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>'
+	$('head').prepend('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>\
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>'
 	);
-
 
 	$('body').append(
 		'<script>\
@@ -20,7 +14,6 @@ $(document).ready(function(){
 		});\
 		</script>'
 	);
-
 
 
     //var hashmap = db_reader.getHashMap();
@@ -41,24 +34,21 @@ function addImage(context, counter) {
 	var word;
 	var reg = /[A-Z]+[a-z]*/gm;
 	while(word = reg.exec(body)){
-		console.log("while");
 			if(word == sarko){
-				console.log(word);
 					var image = '<a href="#" data-toggle="popover" title="Popover Header" data-content="Florian a une petite bite" id="popover'+counter.i+'" class="politicianFind">\
 						<img alt="sarko" src="https://s15.postimg.org/pei4ci3fv/fdp.png" class="politicianFind">\
 					</a>';
-					console.log("Replacing HTML");
 					$(context).html(body.replace(word, word + " " + image));
-					counter.i++;
-					$("#popover"+counter.i).on("click",function(){
-						// Select a specified element
-						$('#popover'+counter.i).show();
-						console.log("Click");
-						window.scrollTo(0,0);
+					chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+					  console.log(response.farewell);
 					});
+					counter.i++;
 			}
 	}
 }
+
+
+
 
 function openModal(word){
     // TODO check db if match
