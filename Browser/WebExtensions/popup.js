fc0801171e7b2fd84f5df12ea311e627cd7d81bc
Florian Felten <florian.felten@student.uclouvain.be>
2016-10-27 17:59:37 +0200

    message passing is working

--- Browser/WebExtensions/popup.js
+++ Browser/WebExtensions/popup.js
@@ -1,21 +1,17 @@
 
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log("Sending answer");
+		sendResponse({farewell: "goodbye"});
+	}
+);
+
 $('#highlight-button').click(launchProcess);
 
 console.log("Ready");
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log("Sending answer");
-    sendResponse({farewell: "goodbye"});
-    return true;
-  });
-);
-
-  function launchProcess() {
-      // Call the method to trigger the search in the page.
-      $('#content-main').append('<span id="politicians"></span>');
-      return;
-  }
-
-
-});
+function launchProcess() {
+	// Call the method to trigger the search in the page.
+	$('#content-main').append('<span id="politicians"></span>');
+	return;
+}
