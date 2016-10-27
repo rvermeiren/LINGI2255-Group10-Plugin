dec6e2666c7e5979d34f758b1c2e3c7b8309cf51
Florian Felten <florian.felten@student.uclouvain.be>
2016-10-27 16:57:26 +0200

    Popover works

--- Browser/WebExtensions/main.js
+++ Browser/WebExtensions/main.js
@@ -1,31 +1,10 @@
-﻿// import * as Papa from "node_modules/papaparse/papaparse.min"
-//import * as db_reader from "utils/db_reader";
-
-//import * as db_reader from "utils/db_reader";
-
-var sarko = "Nicolas";
 
 $(document).ready(function(){
-	console.log('Document is ready.. scanning for politicians...');
-
-	$('head').prepend('<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>'
-	);
-
-
-	$('body').append(
-		'<script>\
-		$(document).ready(function(){\
-		    $(\'[data-toggle="popover"]\').popover();\
-		});\
-		</script>'
-	);
-
-
-
+	var name = "Nicolas";
+	// console.log('Document is ready.. scanning for politicians...');
     //var hashmap = db_reader.getHashMap();
 
-		var counter = {i: 0}; //Occurences. Singleton to be passed by reference and not by value.
+	var counter = {i: 0}; //Occurences. Singleton to be passed by reference and not by value.
     $('p').each(function(index) {
 			addImage(this, counter);
 	  });
@@ -34,28 +13,29 @@ $(document).ready(function(){
 			addImage(this, counter)
     });
 
+	$('head').append(
+		"<script>$(function(){$('[data-toggle=\"popover\"]').popover();});\
+		</script>"
+	);
+
 });
+
+
 
 function addImage(context, counter) {
 	var body = $(context).text();
 	var word;
 	var reg = /[A-Z]+[a-z]*/gm;
 	while(word = reg.exec(body)){
-		console.log("while");
-			if(word == sarko){
-				console.log(word);
-					var image = '<a href="#" data-toggle="popover" title="Popover Header" data-content="Florian a une petite bite" id="popover'+counter.i+'" class="politicianFind">\
-						<img alt="sarko" src="https://s15.postimg.org/pei4ci3fv/fdp.png" class="politicianFind">\
+		// console.log("while");
+			if(word == name){
+				// console.log(word);
+					var image = '<a href="#" data-toggle="popover" title="Popover Header" data-content="A wonderful content" id="popover'+counter.i+'" class="politicianFind">\
+						<img alt="name" src="https://s15.postimg.org/pei4ci3fv/fdp.png">\
 					</a>';
-					console.log("Replacing HTML");
+					// console.log("Replacing HTML");
 					$(context).html(body.replace(word, word + " " + image));
 					counter.i++;
-					$("#popover"+counter.i).on("click",function(){
-						// Select a specified element
-						$('#popover'+counter.i).show();
-						console.log("Click");
-						window.scrollTo(0,0);
-					});
 			}
 	}
 }
