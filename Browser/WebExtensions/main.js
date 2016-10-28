27d4a5ceffa2ce95643b065d956e4a9acd3138d7
Groulard Quentin <quentin.groulard@sutdent.uclouvain.be>
2016-10-28 10:14:16 +0200

    Search in hashmap

--- Browser/WebExtensions/main.js
+++ Browser/WebExtensions/main.js
@@ -29,39 +29,40 @@ function addImage(context, counter) {
 	var word;
 	var reg = /[A-Z]+[a-z]*/gm;
 	while(word = reg.exec(body)){
-
-		if(word == name){
-			var html = "\
-			<div class='panel-body'>\
-				<div class='row'>\
-					<div class='col-xs-2' id='photo'> <i class='material-icons md-60'>face</i> </div>\
-					<div class='col-xs-10'>\
-						<div class='row'>\
-							FN\
-						</div>\
-						<div class='row'>\
-							69/05/1969\
-						</div>\
-						<div class='row'>\
-							Houte-Si-Ploue\
-						</div>\
-						<div class='row'>\
-							<a href='https://www.youtube.com/watch?v=AmYrTp-JdnY'>Voir sur wecitizens</a>\
+		for (var n in hashmap){
+			if (n == word){
+				var html = "\
+				<div class='panel-body'>\
+					<div class='row'>\
+						<div class='col-xs-2' id='photo'> <i class='material-icons md-60'>face</i> </div>\
+						<div class='col-xs-10'>\
+							<div class='row'>\
+								FN\
+							</div>\
+							<div class='row'>\
+								69/05/1969\
+							</div>\
+							<div class='row'>\
+								Houte-Si-Ploue\
+							</div>\
+							<div class='row'>\
+								<a href='https://www.youtube.com/watch?v=AmYrTp-JdnY'>Voir sur wecitizens</a>\
+							</div>\
 						</div>\
 					</div>\
-				</div>\
-			</div>"
+				</div>"
 
-			var image = String('<span id="popoverWeCitizens"><img data-toggle="popover" title="Nom du politicien" data-trigger="hover" id="popover')
-			+ counter.i + String('"data-html="true" src="https://s15.postimg.org/pei4ci3fv/fdp.png" class="politicianFind" data-content="')
-			+ html + String('"></span>');
-			console.log(image);
-			$(context).html(body.replace(word, word + " " + image));
-			chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-			  console.log(response.farewell);
-			});
-			counter.i++;
+				var image = String('<span id="popoverWeCitizens"><img data-toggle="popover" title="Nom du politicien" data-trigger="hover" id="popover')
+				+ counter.i + String('"data-html="true" src="https://s15.postimg.org/pei4ci3fv/fdp.png" class="politicianFind" data-content="')
+				+ html + String('"></span>');
+				console.log(image);
+				$(context).html(body.replace(word, word + " " + image));
+				chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+				  console.log(response.farewell);
+				});
+				counter.i++;
 			}
+		}
 	}
 }
 
