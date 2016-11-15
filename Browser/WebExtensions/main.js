a58b427e2973bf5dfc8708bda85259a3f40bb0df
Florian Felten <florian.felten@student.uclouvain.be>
2016-11-15 15:52:47 +0100

    zizi

--- Browser/WebExtensions/main.js
+++ Browser/WebExtensions/main.js
@@ -34,42 +34,6 @@ function textNodesUnder(el){
 	return a;
 }
 
-function replaceNode(textNode, eNode) {
-    var pNode = textNode.parentNode;
-    pNode.replaceChild(textNode, eNode);
-}
-
-function decorateText(str) {
-    var e = document.createElement("span");
-    e.style.color = "#ff0000";
-    e.appendChild(document.createTextNode(str));
-    return e;
-}
-
-function searchAndReplaceElement(elem) {
-    for(var i = elem.childNodes.length; i--;) {
-        var childNode = elem.childNodes[i];
-        if(childNode.nodeType == 3) { // 3 => a Text Node
-            var strSrc = childNode.nodeValue; // for Text Nodes, the nodeValue property contains the text
-            var strSearch = "Special String";
-            var pos = strSrc.indexOf(strSearch);
-
-            if(pos >= 0) {
-                var fragment = document.createDocumentFragment();
-
-                if(pos > 0)
-                    fragment.appendChild(document.createTextNode(strSrc.substr(0, pos)));
-
-                fragment.appendChild(decorateText(strSearch));
-
-                if((pos + strSearch.length + 1) < strSrc.length)
-                    fragment.appendChild(document.createTextNode(strSrc.substr(pos + strSearch.length + 1)));
-
-                elem.replaceChild(fragment, childNode);
-            }
-        }
-    }
-}
 
 	/* Search through <p> and <li> items TODO add other types*/
 function launchSearch(hashmap){
@@ -79,37 +43,12 @@ function launchSearch(hashmap){
 
 	var arr = textNodesUnder(document.body);
 	for (var i = 0; i < arr.length; i++) {
-		var tab = [];
-		// console.log(arr[i]);
 		for (var j = 0; j < arr[i].childNodes.length; j++){
 			if (arr[i].childNodes[j].nodeType == Node.TEXT_NODE){
-				console.log("Array[i] =");
-				console.log(arr[i]);
-				tab.push({"index": j, "content" : addImage(arr[i], j, arr[i].childNodes[j], counter)});
+				j+=addImage(arr[i], j, arr[i].childNodes[j], counter);
 			}
 		}
-
-
-		var tmp = arr[i];
-		for (var j = tab.length-1; j>=0; j--){
-			var replacementNode = document.createElement('span');
-			replacementNode.innerHTML = tab[j].content;
-			tmp.insertBefore(replacementNode, tmp.childNodes[tab[j].index]);
-			tmp.removeChild(tmp.childNodes[tab[j].index+1]);
-		}
-		arr[i].nodeValue = tmp;
 	}
-	// $('p').each(function(index) {
-	// 		addImage(this, counter);
-	// });
-	//
-	// $('li').each(function(index) {
-	// 		addImage(this, counter);
-	// });
-	//
-    // $('a').each(function(index) {
-	// 		addImage(this, counter);
-	// });
 
 
 	$('head').append(
@@ -163,6 +102,106 @@ function urlBuild(name, firstname, id){
 	return("http://directory.wecitizens.be/fr/politician/" + res + "-" + id);
 }
 
+/* Returns indicates if we can hot swap the text or if we hae to retrun the html
+This is useful to escape balises */
+function addImage(parent, nodeIndex, context, counter) {
+	var toDisplay = [];
+    var body = context.textContent;
+	// console.log(body);
+	var prev;
+	var pref;
+	var word;
+	var prefix = ["", "den ", "der ", "de ", "van "];
+	var reg = /[A-Z]+[a-z]*/gm;
+	var i = 0;
+	// for(i; i<word.length; i++) {
+	while(word = reg.exec(body)){
+		if (word == "De" || word == "Van" || word == "Di" || word == "Vanden" || word == "Ver"){		//Di Rupo rpz
+			pref = word;
+			continue;
+		}
+		var name;
+		var found = false;
+		var iter = 0;
+		while (!found && iter < prefix.length) {
+			if (pref != null){
+				name = pref + " " + prefix[iter] + word;
+			}else{
+				name = prefix[iter] + word;
+			}
+			if (name in hashmap){
+				if(!font){
+					$('head').append('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">');
+					font = true;
+				}
+
+				found = true;
+				var matching = [];
+				var pol = null;
+				for (var i in hashmap[name]){
+					matching.push(hashmap[name][i]);
+					if (prev == hashmap[name][i][4]){		//Matching also firstname
+						pol = i;
+					}
+				}
+				if (pol != null) {
+					console.log("push " + word + " ");
+					toDisplay.push({"index" : reg.lastIndex, "image" : display(hashmap, name, pol, counter, context)});
+				} else {		//Multiple matches
+					console.log("push " + word);
+					toDisplay.push({"index" : reg.lastIndex, "image" : display_multiple(hashmap, name, counter, context)});
+				}
+                counter.i++;
+                pref = null;
+			}
+			prev = word;
+			iter++;
+		}
+	}
+	if(toDisplay.length>0){
+		var ret = context.nodeValue;
+		console.log(ret);
+		var fragment = document.createDocumentFragment();
+		for(var i = 0; i < toDisplay.length; i++) {
+			var icon = toDisplay.pop();
+			console.log(icon.index);
+
+			//End
+			if((icon.index + 1) < ret.length)
+				fragment.insertBefore(document.createTextNode(ret.substr(icon.index + 1)), fragment.firstChild);
+
+			//Image
+			var toAdd = document.createElement("span");
+			toAdd.innerHTML = icon.image;
+			fragment.insertBefore(toAdd, fragment.firstChild);
+
+			//Beginning
+			if(i == toDisplay.length-1){
+				console.log('zizi');
+				fragment.insertBefore(document.createTextNode(ret.substr(0, icon.index)), fragment.firstChild);
+			}
+			ret = ret.substr(0, icon.index);
+		}
+		parent.replaceChild(fragment, context);
+		return toDisplay.length;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function display(hashmap, name, index, counter, context, returns){
     var body = $(context).text();
@@ -205,17 +244,17 @@ function display(hashmap, name, index, counter, context, returns){
 	var image = String('<span id="popoverWeCitizens"><img data-toggle="popover" title="') + hashmap[name][index][4] + " " + hashmap[name][index][5] + String('" id="popover')
 	+ counter.i + String('"data-html="true" src="http://s12.postimg.org/bqsrifs6l/image.png" class="politicianFind" data-content="')
 	+ html + String('"></span>');
-	//console.log(image);
+
+
 	politicianInfos.push({name: hashmap[name][index][4], surname: hashmap[name][index][5], birthDate: bdate,
 		politicalParty: hashmap[name][index][2], city: hashmap[name][index][7], job: hashmap[name][index][8]});
+
 	return image;
-	// $(context).html($(context).html().replace(name, name + " " + image));
 }
 
 
 function display_multiple(hashmap, name, counter, context){
     var body = $(context).text();
-	// console.log(context);
 	var html = "<div class='container' id='content-main'>\
 		<div class='panel-group' id='accordion'>";
 	for(i = 0; i < hashmap[name].length; i++){
@@ -265,71 +304,4 @@ function display_multiple(hashmap, name, counter, context){
 	+ html + String('"></span>');
 	return image;
 	// $(context).html($(context).html().replace(name, name + " " + image));
-}
-
-
-
-/* Returns indicates if we can hot swap the text or if we hae to retrun the html
-This is useful to escape balises */
-function addImage(parent, nodeIndex, context, counter) {
-	var toDisplay = [];
-    var body = context.textContent;
-	// console.log(body);
-	var prev;
-	var pref;
-	var word;
-	var prefix = ["", "den ", "der ", "de ", "van "];
-	var reg = /[A-Z]+[a-z]*/gm;
-	var i = 0;
-	// for(i; i<word.length; i++) {
-	while(word = reg.exec(body)){
-		if (word == "De" || word == "Van" || word == "Di" || word == "Vanden" || word == "Ver"){		//Di Rupo rpz
-			pref = word;
-			continue;
-		}
-		var name;
-		var found = false;
-		var iter = 0;
-		while (!found && iter < prefix.length) {
-			if (pref != null){
-				name = pref + " " + prefix[iter] + word;
-			}else{
-				name = prefix[iter] + word;
-			}
-			if (name in hashmap){
-				console.log("Found " + word + " at index " + word.index);
-				if(!font){
-					$('head').append('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">');
-					font = true;
-				}
-
-				found = true;
-				var matching = [];
-				var pol = null;
-				for (var i in hashmap[name]){
-					matching.push(hashmap[name][i]);
-					if (prev == hashmap[name][i][4]){		//Matching also firstname
-						pol = i;
-					}
-				}
-				if (pol != null) {
-					toDisplay.push({"index" : word.index + word.length, "image" : display(hashmap, name, pol, counter, context)});
-				} else {		//Multiple matches
-					toDisplay.push({"index" : word.index + word.length, "image" : display_multiple(hashmap, name, counter, context)});
-				}
-                counter.i++;
-                pref = null;
-			}
-			prev = word;
-			iter++;
-			// i++;
-		}
-	}
-	var ret = parent.childNodes[nodeIndex].nodeValue;
-	for(var i = 0; i < toDisplay.length; i++) {
-		var icon = toDisplay.pop();
-		ret=ret.substr(0, icon.index) + icon.image + ret.substr(icon.index + 1);
-		// context.textContent = body.substr(0, icon.index) + icon.image + body.substr(icon.index + 1);
-	}
-	return ret;
 }
