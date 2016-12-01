7a45b42521153c16e3806301322eaa50148829aa
Florian Felten <florian.felten@student.uclouvain.be>
2016-12-01 12:00:12 +0100

    clean background.js

--- Browser/WebExtensions/scripts/background.js
+++ Browser/WebExtensions/scripts/background.js
@@ -1,3 +1,14 @@
+/*
+ * background.js: This document manages the badge and notifications of the
+ * extension. It sends a message to the content script every seconds to know
+ * the number of politicians spotted. Then it sets the badge that appears over
+ * the plugin icon on the upper right of the browser. If it is a pdf document,
+ * it will make a notification appear as well when the page is loaded
+ */
+
+
+// This counts the number of politicians encountered on webpages
+// There is one count per tab
 var counts = {}
 
 /* Listens for tabs changes and updates the badge if it changes*/
@@ -7,6 +18,7 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
     badgeMessage();
 });
 
+// dismiss the notification when clicking on it
 chrome.notifications.onClicked.addListener(function(Id) {
 	chrome.notifications.clear(Id, function(){});
 });
@@ -14,7 +26,7 @@ chrome.notifications.onClicked.addListener(function(Id) {
 /* This updates the badge every seconds*/
 window.setInterval(function(){
 	badgeMessage();
-}, 4000);
+}, 1000);
 
 /* Sends a message to the tab to know how many politicians were found*/
 function badgeMessage() {
@@ -22,11 +34,11 @@ function badgeMessage() {
 		active: true,
 		currentWindow: true
 	}, function (tabs) {
+		// query the content script
 		chrome.tabs.sendMessage(
 			tabs[0].id,
 			{from: 'popup', subject: 'badge'},
-			function(request){
-				// alert(request.notification + " "+ tabs[0].id)
+			function(request){ // on response received
 				badge(request, tabs[0].id);
 			});
 	});
@@ -34,18 +46,18 @@ function badgeMessage() {
 
 /* Updates the badge and sets a notification if necessary*/
 function badge(request, id) {
+	// if the document is a pdf and the counter has been updated and there are politicians
 	if (request.notification == true && request.count != counts[id.toString()] && request.count != 0) {
 		notification(request.count);
 	}
+	// updates the counter of this tab
 	counts[id.toString()] = request.count;
-	// alert(request.count.toString());
-	console.log(request);
+	// sets the badge
 	chrome.browserAction.setBadgeText({text: request.count.toString()})
 }
 
 /* Displays a notification with how many politicians were found in the pdf*/
 function notification(count) {
-	console.log("HERE");
 	chrome.notifications.create(
         'pol_found',{
             type:"basic",
