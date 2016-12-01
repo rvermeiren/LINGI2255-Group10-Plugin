a8b29be3f99bd8ab33d62708b6ddf44e6bb0ebf7
Jerome Thiry <jr.thiry@gmail.com>
2016-12-01 12:04:35 +0100

    Cleaned popup.js

--- Browser/WebExtensions/scripts/popup.js
+++ Browser/WebExtensions/scripts/popup.js
@@ -1,11 +1,19 @@
-//Once the DOM is ready
+/** Popup.js
+ * This script fills the extension popup with the politicians that were found on
+ * the page.
+ * It sends a message to the current page when opening the popup to retrieve the
+ * list of politicians.
+ */
+
+//Send a message to the tab when the popup is opened
 window.addEventListener('DOMContentLoaded', function () {
 	document.getElementById("checkbox").addEventListener("click", function(){
     	update_research(document.getElementById("checkbox"));
 	});
-
+	// "Search" in local storage tell us if the search is activated or not
 	var retrievedObject = chrome.storage.local.get('search',
 		function(result){
+			// Check if "Search" is present in local storage"
 			if(JSON.stringify(result) != '{}')
 				document.getElementById("checkbox").checked = result.search
 			else document.getElementById("checkbox").checked = true
@@ -17,40 +25,25 @@ window.addEventListener('DOMContentLoaded', function () {
 		active: true,
 		currentWindow: true
 	}, function (tabs) {
-	//Send a request for the HTML info
+	//Send a request for the politicians info
 		chrome.tabs.sendMessage(
 	   	 	tabs[0].id,
 	   		{from: 'popup', subject: 'politicianInfos'},
-	   		//Specifying a callback to be called from the receiving end (content script)
+	   		//Execute ReplaceHTML with our list
 	    	replaceHTML);
-		console.log('Message sent from background.js');
 	});
-
 });
 
+// Listener on the checkbox - activate or deactivate the search
 function update_research(cb) {
-	chrome.storage.local.set({'search': cb.checked},
-		function(){
-			console.log("Search setted to: "+ cb.checked);
-		});
+	chrome.storage.local.set({'search': cb.checked},function(){});
 }
 
-
-function containsObject(obj, array) {
-	var obj = JSON.stringify(obj);
-
-    for (var i = 0; i < array.length; i++) {
-        if (JSON.stringify(array[i]) === obj) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-// Update the relevant fields with the new data
+// Update the HTML of the popup with the politicians list
 function replaceHTML(politicianInfos) {
 
 	var count = 0;
+	// We received a hashmap with the politians - i is in format FirstnameLastname
 	for (var i in politicianInfos) {
 
 		var name			= politicianInfos[i].name;
@@ -62,6 +55,7 @@ function replaceHTML(politicianInfos) {
 		var photo			= politicianInfos[i].photo;
 		var link			= politicianInfos[i].link;
 
+		// Append a panel for every politician in content-main
 		$('#content-main').append('<div class="panel-group" id="accordion'+count+'">\
 				                <div class="panel panel-default"">\
 				                    <div class="panel-heading">\
